e05b657ba5f57613999feac5780a0552bbdaa2cb
virak Sim <simvirak210721@gmail.com>
2024-12-14 09:36:13 +0700

    feat: header and navbar

---
+++ src/app/service/page.tsx
@@ -0,0 +1,9 @@
+const SerVice = () => {
+  return (
+    <div>
+      <h2>sevice page</h2>
+    </div>
+  );
+};
+
+export default SerVice;
---
+++ src/constants/navbar.tsx
@@ -0,0 +1,22 @@
+export const NavBarLink = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "service",
+    path: "/service",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
