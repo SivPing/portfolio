5982cca4ab14bdd07f452d2be8bc4f93fcdd5f01
virak Sim <simvirak210721@gmail.com>
2024-12-16 14:09:10 +0700

    feat: hero-section

---
+++ src/constants/status.tsx
@@ -0,0 +1,18 @@
+export const STATUS = [
+  {
+    number: 2,
+    name: "Year of experience",
+  },
+  {
+    number: 18,
+    name: "Projects completed",
+  },
+  {
+    number: 7,
+    name: "Technologies mastered",
+  },
+  {
+    number: 300,
+    name: "Code Commits",
+  },
+];
