2a76ccbe0e337d154fc4b23824d9f9c7d5e9f0f3
virak Sim <simvirak210721@gmail.com>
2024-12-16 15:39:33 +0700

    feat:service-section

---
+++ src/constants/service.tsx
@@ -0,0 +1,32 @@
+export const SERVICE = [
+  {
+    number: 1,
+    title: "Web Development",
+    description:
+      "I specialize in web development, creating dynamic and interactive web applications using cutting-edge technologies and frameworks.",
+    href: "",
+  },
+  {
+    number: 2,
+
+    title: "Backend Development",
+    description:
+      "I also have experience in backend development, building robust and scalable server-side systems for web applications.",
+    href: "",
+  },
+  {
+    number: 3,
+
+    title: "Mobile App Development",
+    description:
+      "I also have expertise in mobile app development, creating user-friendly and engaging mobile apps for Android and iOS platforms.",
+    href: "",
+  },
+  {
+    number: 4,
+    title: "UI/UX Design",
+    description:
+      "I also have experience in UI/UX design, designing intuitive and visually appealing user interfaces for web and mobile applications.",
+    href: "",
+  },
+];
