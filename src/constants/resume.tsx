4a875c73aff1f57a58598e971ab8d5e284a4508b
virak Sim <simvirak210721@gmail.com>
2024-12-17 14:14:51 +0700

    feat: resume

---
+++ src/constants/resume.tsx
@@ -0,0 +1,166 @@
+import { FaCss3Alt } from "react-icons/fa";
+import { PiFileHtmlFill } from "react-icons/pi";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { SiNextdotjs } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiNestjs } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+
+export const RESUME = [
+  {
+    title: " About Me",
+    descripion:
+      "Computer Science Student. Iconsider my self a responsible and orderly person.I am looking forward for my worke xperience .",
+    info: [
+      { fielName: "Name", fielValue: "Virak Sim" },
+      { fielName: "Phone", fielValue: "(+855) 93 821 749" },
+      { fielName: "Experience", fielValue: "2+ Years" },
+      { fielName: "Skype", fielValue: "Virak." },
+      { fielName: "Nationalify", fielValue: "Cambodia" },
+      { fielName: "Email", fielValue: "sim210721@gmail.com" },
+      { fielName: "Freelance", fielValue: "Available" },
+      { fielName: "Languages", fielValue: "English, Khmer" },
+    ],
+  },
+];
+
+export const EXPERIENCE = [
+  {
+    title: "My Experience",
+    description:
+      " Experienced Front-end Developer with over 2 years of experience in Vue.js custom theme building and editing. Proficient in web technologies and WordPress platform, with expertise in Elementor",
+
+    items: [
+      {
+        duration: "DreamsLAB-kh",
+        position: "Junior Web Developer",
+        year: "2024 - Present",
+      },
+      {
+        duration: "E-Commerce",
+        position: "Freelance Web Developer",
+        year: "2021 - 2024",
+      },
+
+      {
+        duration: "2M-Media",
+        position: "Frontend Developer",
+        year: "2022 - 2024",
+      },
+
+      {
+        duration: "2M-Media",
+        position: "Digital Marketing",
+        year: "2020 - 2021",
+      },
+      {
+        duration: "2M-Media",
+        position: "Frontend Developer",
+        year: "2022 - 2024",
+      },
+
+      {
+        duration: "2M-Media",
+        position: "Digital Marketing",
+        year: "2020 - 2021",
+      },
+    ],
+  },
+];
+
+export const EDUCATION = [
+  {
+    title: "My Education",
+    description:
+      " ROYAL UNIVERSITY OF PHNOM PENH Computer Science Department, in progress Year 4 ",
+    items: [
+      {
+        institution: "Royal University of Phnom Penh",
+        degree: "Computer Science",
+        year: "2020 - 2024",
+      },
+      {
+        institution: "Udemy",
+        degree: "Fullstack Web Development",
+        year: "2024 - Present",
+      },
+      {
+        institution: "TFD",
+        degree: "Fullstack Web Development",
+        year: "2024",
+      },
+      {
+        institution: "Online Course",
+        degree: "API Development",
+        year: "2023",
+      },
+      {
+        institution: "TFD",
+        degree: "Fullstack Web Development",
+        year: "2024",
+      },
+      {
+        institution: "Online Course",
+        degree: "API Development",
+        year: "2023",
+      },
+    ],
+  },
+];
+
+export const SKILLS = [
+  {
+    title: "My Skills",
+    description:
+      "Web developer skills are the foundation of building functional, interactive, and accessible websites that power modern digital experiences. As technology advances, these skills remain in high demand, offering opportunities to innovate, create, and make a meaningful impact in various industries.",
+    skillList: [
+      {
+        icons: <PiFileHtmlFill />,
+        name: "HTML",
+        value: "90 %",
+      },
+      {
+        icons: <FaCss3Alt />,
+        name: "CSS",
+        value: "90 %",
+      },
+      {
+        icons: <IoLogoJavascript />,
+        name: "JavaScript",
+        value: "90 %",
+      },
+      {
+        icons: <SiTypescript />,
+        name: "TypeScript",
+        value: "90 %",
+      },
+      {
+        icons: <FaReact />,
+        name: "React",
+        value: "90 %",
+      },
+      {
+        icons: <SiNextdotjs />,
+        name: "Next.js",
+        value: "90 %",
+      },
+      {
+        icons: <RiTailwindCssFill />,
+        name: "Tailwind CSS",
+        value: "90 %",
+      },
+      {
+        icons: <SiNestjs />,
+        name: "Nest js",
+        value: "80 %",
+      },
+      {
+        icons: <SiMongodb />,
+        name: "MongoDB",
+        value: "75 %",
+      },
+    ],
+  },
+];
