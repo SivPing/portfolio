cfd3e1e2700a18d609e81ea572ba6a422b627415
virak Sim <simvirak210721@gmail.com>
2024-12-20 11:48:37 +0700

    feat/projects-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/module/home/hero";
 import Service from "./service/page";
 import Resume from "./resume/page";
-import Work from "./work/page";
+import Project from "./projects/page";
 
 const Home = () => {
   return (
@@ -9,7 +9,7 @@ const Home = () => {
       <Hero />
       <Service />
       <Resume />
-      <Work />
+      <Project />
     </section>
   );
 };
